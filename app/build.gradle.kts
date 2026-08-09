074bb042644993d54ffdfa08ffa1cc9e34fdb0f7
zephyr4289 <zephyr4289@gmail.com>
2026-08-10 06:37:24 +0900

    Fix duplicate META-INF INDEX.LIST and add spirv-headers for ggml-vulkan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.4"
     }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/INDEX.LIST"
+        }
+    }
 }
 
 dependencies {
